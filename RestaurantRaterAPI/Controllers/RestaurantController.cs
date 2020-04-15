2cc7665e016b7c6cfb32ca54d1500519f59aa620
Greg Murray <gremurra@gmail.com>
2020-04-15 10:12:53 -0400

    update endpoint added

--- RestaurantRaterAPI/Controllers/RestaurantController.cs
+++ RestaurantRaterAPI/Controllers/RestaurantController.cs
@@ -16,7 +16,8 @@ namespace RestaurantRaterAPI.Controllers
 
         //starting by saying what type of methods these are
         //must create an object before you do anything with it
-        [HttpPost]
+        
+        [HttpPost]   //Create in CRUD Example
         public async Task<IHttpActionResult> PostRestaurant(Restaurant model)
         {
             //because of the type of method, you can see if the model being passed in is valid
@@ -32,11 +33,47 @@ namespace RestaurantRaterAPI.Controllers
             return BadRequest(ModelState);
         }
 
-        [HttpGet]
+        //GET ALL
+        [HttpGet]   //Read 1 in CRUD Example
         public async Task<IHttpActionResult> Get()
         {
             List<Restaurant> restaurants = await _context.Restaurants.ToListAsync();
             return Ok(restaurants);
+        }
+
+        //GET BY ID
+        [HttpGet]   //Read 2 in CRUD Example
+        public async Task<IHttpActionResult> GetById(int id)
+        {
+            //Need to pass in ID
+            Restaurant restaurant = await _context.Restaurants.FindAsync(id);
+            //searching entire database for that one object
+            if (restaurant != null)
+            {
+                return Ok(restaurant);
+            }
+            return NotFound();
+        }
+
+        [HttpPut]    //Update in CRUD example
+
+        public async Task<IHttpActionResult> UpdateRestaurant([FromUri] int id, [FromBody] Restaurant model)
+        {
+            if (ModelState.IsValid)
+            {                                                       //FindAsync method returns null if it doesnt work
+                Restaurant restaurant = await _context.Restaurants.FindAsync(id);
+                if (restaurant != null)
+                {
+                    restaurant.Name = model.Name;
+                    restaurant.Address = model.Address;
+                    restaurant.Rating = model.Rating;
+
+                    await _context.SaveChangesAsync();
+                    return Ok();    //this is the 200 on Postman
+                }
+                return NotFound();
+            }
+            return BadRequest(ModelState);
         }
     }
 }
